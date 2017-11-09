125d6635bd5d0f49ff83ad308971578633dda703
Koki Nagatani <nagatani@Koki-no-MacBook-Pro.local>
2017-11-09 22:13:36 +0900

    Initial Commit

---
+++ FindIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindIt
+//
+//  Created by Koki Nagatani on 2017/11/09.
+//  Copyright © 2017年 Koki Nagatani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
